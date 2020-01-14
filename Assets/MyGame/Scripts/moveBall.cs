dd7521f735320d2f5e7ea80e145b2e3520feac19
Melissa Illig <melissaillig@melissas-mbp.home>
2020-01-14 18:37:57 +0100

    update GUI, add Script

---
+++ Assets/MyGame/Scripts/moveBall.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+
+public class moveBall : MonoBehaviour
+{
+  [SerializeField]  private GameObject[] ballPositions;
+
+
+  public GameObject Ball;
+  int  PosRandom;
+  int PosBefore;
+
+  void SetBallRandom () {
+      PosRandom = 0;
+      PosBefore = 4;
+  }
+
+
+    void SetPosition () {
+        Ball.transform.position = ballPositions[PosRandom].transform.position;
+
+    }
+
+    
+public void getPosBefore(){
+PosRandom = Random.Range(0,4);
+    if (PosRandom != PosBefore)
+{
+PosBefore = PosRandom;
+SetPosition();
+}
+else {
+    getPosBefore();
+}
+
+}
+   
+
+}
+
+
+
+
+
+
+
+
+
+
